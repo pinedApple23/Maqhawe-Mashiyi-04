353f75c8e6f7abfd7ae65cf9126e01ce521335d1
Mashiyi M <142584237+pinedApple23@users.noreply.github.com>
2025-04-20 13:39:06 +0200

    Add files via upload

---
+++ src/InterviewPrep.js
@@ -0,0 +1,19 @@
+class InterviewTip {
+    constructor(id, title, content){
+    
+    this.id = id;
+    this.title = title;
+    this.content = content;
+   }
+  
+    addContent(newContent){
+    this.content = newContent;
+   }
+   removeContent(){
+    this.content = content;
+   }
+   updateContent(newContent){
+   this.content = newContent;
+  }
+  }
+  
---
+++ src/User.js
@@ -0,0 +1,17 @@
+class User{
+    constructor(id, name, email, password, role){
+       this.id = id;
+       this.name = name;
+       this.email = email;
+       this.password = password;
+       this.role = role;
+      }
+    
+      login(){
+    
+      }
+      logout(){
+    
+      }
+    }
+    
