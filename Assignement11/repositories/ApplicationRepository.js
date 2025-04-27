e20d418175faeb3435544729a3954202ba111827
Mashiyi M <142584237+pinedApple23@users.noreply.github.com>
2025-04-27 18:32:54 +0200

    Add files via upload

---
+++ Assignement11/repositories/ApplicationRepository.js
@@ -0,0 +1,7 @@
+const Repository = require('./Repository');
+
+class ApplicationRepository extends Repository {
+  // Extend in future (e.g., findByJobPostId, findByUserId)
+}
+
+module.exports = ApplicationRepository;
