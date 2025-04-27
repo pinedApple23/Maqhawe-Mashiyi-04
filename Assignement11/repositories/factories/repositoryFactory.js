7139b2f8124f790b575b197cf091b86466dd304d
Mashiyi M <142584237+pinedApple23@users.noreply.github.com>
2025-04-27 18:34:38 +0200

    Add files via upload

---
+++ Assignement11/repositories/factories/repositoryFactory.js
@@ -0,0 +1,42 @@
+const InMemoryUserRepository = require('../repositories/inmemory/InMemoryUserRepository');
+const InMemoryJobPostRepository = require('../repositories/inmemory/InMemoryJobPostRepository');
+const InMemoryApplicationRepository = require('../repositories/inmemory/InMemoryApplicationRepository');
+
+// Future: You can add imports for FileSystemUserRepository, DatabaseUserRepository, etc.
+
+class RepositoryFactory {
+  static getUserRepository(storageType = 'MEMORY') {
+    switch (storageType) {
+      case 'MEMORY':
+        return new InMemoryUserRepository();
+      // case 'FILESYSTEM':
+      //   return new FileSystemUserRepository();
+      default:
+        throw new Error(`Unknown storage type: ${storageType}`);
+    }
+  }
+
+  static getJobPostRepository(storageType = 'MEMORY') {
+    switch (storageType) {
+      case 'MEMORY':
+        return new InMemoryJobPostRepository();
+      // case 'FILESYSTEM':
+      //   return new FileSystemJobPostRepository();
+      default:
+        throw new Error(`Unknown storage type: ${storageType}`);
+    }
+  }
+
+  static getApplicationRepository(storageType = 'MEMORY') {
+    switch (storageType) {
+      case 'MEMORY':
+        return new InMemoryApplicationRepository();
+      // case 'FILESYSTEM':
+      //   return new FileSystemApplicationRepository();
+      default:
+        throw new Error(`Unknown storage type: ${storageType}`);
+    }
+  }
+}
+
+module.exports = RepositoryFactory;
